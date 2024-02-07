2d9188a8a5ab56693134d83630f920cfb825d17a
Geerish Sumbojee <geerish@omerp.net>
2024-02-07 20:13:50 +0400

    Update postgres_backup.sh

--- postgres_backup.sh
+++ postgres_backup.sh
@@ -36,7 +36,6 @@ if [[ -f $FILE_NAME && ! -z $DROPBOX_API_KEY ]]
 then
     echo "Encrypting backup and sending to Dropbox ..."
     7z a -mhe=on $FILE_NAME.7z $FILE_NAME \
-    && dropbox_uploader upload $FILE_NAME.7z \
     &&
   curl -X POST https://content.dropboxapi.com/2/files/upload \
     --header "Authorization: Bearer $DROPBOX_API_KEY" \
